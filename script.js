b631f29b6779a0d83c1d7a91089d5505845743c1
George <kositskiy2003@mail.ru>
2022-08-07 20:48:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// function countBy(x, n) {
+//     let z = [],
+//         i
+//     for (i = 1; z.length != n; i++) {
+//       if (i % x == 0) {
+//         z.push(i);
+//       } else {
+//         continue;
+//       }
+//     }
+  
+//     return z;
+// }
+
+// console.log(countBy(2, 5));
+
+// function betterThanAverage(classPoints, yourPoints) {
+//     // Your code here
+//     let sr = 0
+//     classPoints.forEach(item => {
+//         sr += item;
+//     });
+//     sr = sr / (classPoints.length);
+//     if (sr > yourPoints) {
+//         return false;
+//     } else {
+//         return true;
+//     }
+//   }
+
+// function sumMix(x){
+//     let sum = 0;
+//     x.forEach(item => {
+//       sum += new Number(item);
+//     });
+//     return sum;
+//   }
+
+// console.log(sumMix([9, 3, '7', '3']))
+
+function century(year) {
+    if (year % 100 == 0) {
+      return year / 100
+    } else {
+      return Math.floor(year / 100) + 1;
+    }
+  }
