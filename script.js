7b4f32d53568c7e854cefabea1e94ab4d4368664
George <kositskiy2003@mail.ru>
2022-08-07 21:50:45 +0300

    completed 5 tasks

--- script.js
+++ script.js
@@ -38,10 +38,28 @@
 
 // console.log(sumMix([9, 3, '7', '3']))
 
-function century(year) {
-    if (year % 100 == 0) {
-      return year / 100
-    } else {
-      return Math.floor(year / 100) + 1;
-    }
-  }
+// function century(year) {
+//     if (year % 100 == 0) {
+//       return year / 100
+//     } else {
+//       return Math.floor(year / 100) + 1;
+//     }
+// }
+
+// const rps = (p1, p2) => {
+//     if (p1 == 'scissors' & p2 == 'paper') {
+//       return 'Player 1 won!';
+//     } else if (p1 == 'paper' & p2 == 'scissors') {
+//       return 'Player 2 won!';
+//     } else if (p1 == 'rock' & p2 == 'scissors') {
+//       return 'Player 1 won!';
+//     } else if (p1 == 'scissors' & p2 == 'rock') {
+//       return 'Player 2 won!';
+//     } else if (p1 == 'rock' & p2 == 'paper') {
+//       return 'Player 2 won!';
+//     } else if (p1 == 'paper' & p2 == 'rock') {
+//       return 'Player 1 won!';
+//     } else {
+//       return 'Draw!';
+//     }
+//   };
